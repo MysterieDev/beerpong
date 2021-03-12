cd430694ef427aa73e1f702f708bd3353c823bc5
Mysteriedev <josefsle97@gmail.com>
2021-03-12 07:06:35 +0100

    logic

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+// PART 1 basics and shoot the ball
 const ball = document.querySelector("#ball");
 const ballDiv = document.querySelector("#ellipse-div");
 const root = document.documentElement;
-ball.classList.add("shootY");
-ball.onclick = changeDirection;
+const screenOffsetX = parseInt(offset(ball).left);
+
+//start game
+nextShot();
 
 function changeDirection() {
     const ballYPosition = offset(ball);
@@ -14,13 +17,29 @@ function changeDirection() {
 }
 
 function shoot() {
+    ball.onclick = "";
+    ball.style.left = calculateXPosition();
+    //current position
     const yPosition = parseInt(ball.style.top);
+    //offset for throw
     const newYPosition = yPosition - 320;
+    //set ball top new position
     ball.style.top = newYPosition;
     root.style.setProperty("--top", yPosition + "px");
     root.style.setProperty("--topShot", newYPosition + "px");
+    //remove x-axis animation
     ball.classList.remove("shootX");
+    //do throw animation
     ball.classList.add("Shoot");
+    setTimeout(evaluateShot, 1100);
+}
+
+function calculateXPosition() {
+    const ballXOffset = parseInt(offset(ball).left);
+    if (ballXOffset > screenOffsetX) {
+        return ballXOffset - screenOffsetX;
+    }
+    return -(screenOffsetX - ballXOffset);
 }
 
 function offset(el) {
@@ -29,4 +48,64 @@ function offset(el) {
         scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
         scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+}
+
+//PART 2 - REMOVE CUPS THAT ARE A HIT
+function evaluateShot() {
+    const ballXOffset = parseInt(offset(ball).left);
+    const ballYOffset = parseInt(offset(ball).top);
+    const cupPositions = [];
+    document
+        .querySelectorAll(".beerCup")
+        .forEach((el) => cupPositions.push({...offset(el), reference: el }));
+
+    cupPositions
+        .filter((cup) => {
+            console.log("top", ballYOffset - cup.top, "left", ballXOffset - cup.left);
+            // Evaluating the shot works by checking the offset from the top and from the left of the screen
+            //To hit a cup we need to be in a certain range close to similar to the cups top and left offset
+            console.log(
+                ballXOffset - cup.left > 0 &&
+                ballXOffset - cup.left < 25 &&
+                ballYOffset - cup.top < 5 &&
+                ballYOffset - cup.top > -20
+            );
+            if (
+                ballXOffset - cup.left > 0 &&
+                ballXOffset - cup.left < 25 &&
+                ballYOffset - cup.top < 5 &&
+                ballYOffset - cup.top > -24
+            ) {
+                return true;
+            } else {
+                return false;
+            }
+        })
+        .map((cup) => {
+            cup.reference.outerHTML = "";
+        });
+}
+
+//Part 3 - restart
+
+function nextShot() {
+    ball.style = "";
+    ball.classList.remove("Shoot");
+    ball.classList.add("shootY");
+    ball.onclick = changeDirection;
+    doesRowStillHaveCups();
+}
+
+function doesRowStillHaveCups() {
+    document.querySelectorAll(".cup-row").forEach((el) => {
+        const remainingCups = el.querySelectorAll(".beerCup");
+        if (remainingCups.length === 0) el.style = "padding-bottom:50px;";
+    });
+} {}
+
+function allCupsShot() {
+    if (document.querySelectorAll(".beerCup").length === 0) {
+        return true;
+    }
+    return false;
 }
